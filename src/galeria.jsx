d4837648ef4f0511e2e0c65f85371933c1636c28
SebastianOkuslki <trajda8@gmail.com>
2025-03-18 13:18:01 +0100

    Human_photos

--- src/galeria.jsx
+++ src/galeria.jsx
@@ -1,3 +1,17 @@
-function Photo(props){
-    return <>witam </>
+function Human_Photos(){
+    const IsVisableHuman= false;
+    const ShowHuman = () =>{
+
+    }
+    return(
+        <>
+            <button onClick={ShowHuman}>Ludzie</button>
+            <div id="Human_Container">
+                <img class="human" src="https://cdn.pixabay.com/photo/2017/10/24/18/43/man-2885709_960_720.png" alt="Typ z psem" />
+                <img class="human" src="https://cdn.pixabay.com/photo/2017/10/24/18/43/man-2885709_960_720.png" alt="Typ z psem" />
+                <img class="human" src="https://cdn.pixabay.com/photo/2017/10/24/18/43/man-2885709_960_720.png" alt="Typ z psem" />
+            </div>
+        </>
+    )
 }
+export default Human_Photos
